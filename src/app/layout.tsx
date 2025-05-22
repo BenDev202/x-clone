00132d82ab5a2112b7d8a73e6c8b43bb4d2088be
armand benjamin <armandbenjamin30@gmail.com>
2025-05-22 17:16:41 +0200

    fix: correct border-x syntax in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto flex justify-between">
           <div className="px-2 xsm:px-4 xxl:px-8"><LeftBar /></div>
-          <div className="flex-1 lg:min-w-[640px] border-x-(1px) border-gray-100">{children}</div>
+          <div className="flex-1 lg:min-w-[640px] border-x-[1px] border-gray-100">{children}</div>
           <div className="hidden lg:flex ml-4 md:ml-8 h-screen"><RightBar /></div>
         </div>
       </body>
