4238471e4daeef7f03366438fc74a59599f5bdac
armand benjamin <armandbenjamin30@gmail.com>
2025-05-23 08:50:01 +0200

    feat: enhance LeftBar component with logo and user section; update layout and Tailwind colors

--- src/app/components/LeftBar.tsx
+++ src/app/components/LeftBar.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 export default function LeftBar() {
   return (
-    <div>LeftBar</div>
+    <div>
+      {/* LOGO MENU BUTTON */}
+      <div className='h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8'>
+        <Link href='/'>
+          <Image src='/logo' alt="logo" width={24} height={24} />
+        </Link>
+      </div>
+      {/* USER */}
+      <div className=''>user</div>
+    </div>
   )
 }
